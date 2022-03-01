2d1dd2dc24823d8127188339f65b9ed60af79340
andro2ss <a.brzeski@hotmail.com>
2022-03-01 05:49:19 -0800

    added header and footer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import { useEffect, useState } from "react";
 import loadDevices from "./functions/loadDevices";
-import SimpleAccordion from "./components/SimpleAccordion";
+import SimpleAccordion from "./components/simpleAccordion/SimpleAccordion";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import FixedBottomNavigation from "./components/FixedBottomNavigation";
 
 function App() {
   const [devices, setDevices] = useState([]);
   const [serverRequestStatus, setServerRequestStatus] = useState("init");
 
   useEffect(() => {
+    console.log(serverRequestStatus);
     loadDevices(setDevices, setServerRequestStatus);
-  }, []);
+    console.log(devices);
+  }, [serverRequestStatus]);
 
   return (
     <div className="App">
-      {devices.length > 1 ? (
-        <SimpleAccordion devices={devices} />
+      <ResponsiveAppBar />
+      {devices.length >= 1 ? (
+        <SimpleAccordion
+          devices={devices}
+          setServerRequestStatus={setServerRequestStatus}
+        />
       ) : (
         "You dont have devices"
       )}
+      <FixedBottomNavigation />
     </div>
   );
 }
