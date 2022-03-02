f704daab8535ae3358866917d9ff8b60332f84e1
andro2ss <a.brzeski@hotmail.com>
2022-03-02 03:35:49 -0800

    fixed bug when removing device

--- src/components/simpleAccordion/devices/multiActionAreaCard/items/CardsActions.js
+++ src/components/simpleAccordion/devices/multiActionAreaCard/items/CardsActions.js
@@ -3,14 +3,20 @@ import { Button, CardActions } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import "./CardsActions.scss";
 import deleteDevice from "../../../../../functions/deleteDevice";
+import { useDispatch } from "react-redux";
+import { setDeviceDetails, setDeviceDetailsId } from "../../../../../actions";
 
 function CardsActions({ deviceId, setServerRequestStatus }) {
+  const dispatch = useDispatch();
+
   return (
     <CardActions className="item__btn">
       <Button
         size="small"
         color="primary"
         onClick={() => {
+          dispatch(setDeviceDetailsId(0));
+          dispatch(setDeviceDetails({}));
           deleteDevice(deviceId, setServerRequestStatus);
         }}
       >
