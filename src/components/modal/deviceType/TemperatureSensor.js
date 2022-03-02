40ccbaf30fa8c52ffc5de97aeed530efee0209f3
andro2ss <a.brzeski@hotmail.com>
2022-03-02 03:37:46 -0800

    fixed name

--- src/components/modal/deviceType/TemperatureSensor.js
+++ src/components/modal/deviceType/TemperatureSensor.js
@@ -4,7 +4,7 @@ function TemperatureSensor({ device }) {
   return (
     <>
       <div className="container__text">
-        <span className="container__text--title">Turned on:</span>
+        <span className="container__text--title">Temperature:</span>
         <span className="container__text--normal">
           {device.temperature} Celsius
         </span>
